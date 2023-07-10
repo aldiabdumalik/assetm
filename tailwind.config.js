216dee3a450a71cd121a57b5e0bac0cbe5b01025
Abdu <aldiabdumalik98@gmail.com>
2023-07-10 09:06:11 +0700

    first

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  important: true,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
