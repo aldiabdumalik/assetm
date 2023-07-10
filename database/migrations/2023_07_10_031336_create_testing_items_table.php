8605a86e62cf72970137511c197b39dbf9db8d26
Abdu <aldiabdumalik98@gmail.com>
2023-07-10 10:19:40 +0700

    testing table

---
+++ database/migrations/2023_07_10_031336_create_testing_items_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTestingItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('testing_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->nullable();
+            $table->foreignId('model_id')
+                ->nullable();
+            $table->string('barcode');
+            $table->tinyInteger('status');
+            $table->string('box_ok');
+            $table->string('box_nok');
+            $table->string('type_desc');
+            $table->string('brand_desc');
+            $table->string('model_desc');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('testing_items');
+    }
+}
