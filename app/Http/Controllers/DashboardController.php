0bc65bcc10b48c5f91f0159007b52740bc06562d
Abdu <aldiabdumalik98@gmail.com>
2023-07-16 10:38:54 +0700

    update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Delivery;
+use App\Models\PackingList;
+use App\Models\ScanningItem;
+use App\Models\TestingItem;
+use App\Models\UserInfo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    public function index() {
-        return view('pages.admin.dashboard');
+    public function index() 
+    {
+        $user = UserInfo::with('branch')->where('user_id', Auth::user()->id)->first();
+        $total_item = ScanningItem::count();
+        $total_ok = TestingItem::where('status', 1)->count();
+        $total_pl = PackingList::count();
+        $total_pengiriman = Delivery::where('status', 1)->count();
+        return view('pages.admin.dashboard', compact('total_item', 'total_ok', 'total_pl', 'total_pengiriman'));
     }
 }
